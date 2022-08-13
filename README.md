2eb93473078c2714c7bcdcb7b584e38cd59830cb
nickxscott <nscott@pratt.edu>
2022-08-13 16:21:08 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# GetBinary
-Web app that converts base 10 integers to base 2 integers
+# What's My Race Pace
+Web application to convert base 10 integers to base 2 integers.
+Deployed app using Heroku. View the app here: https://my-race-pace.herokuapp.com/
---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, flash
+import math
+
+app = Flask(__name__)
+app.secret_key = "thisisthesecretkey"
+
+@app.route('/')
+def index():
+  flash("enter base 10 integer")
+  return render_template('index.html')
+
+@app.route('/GetBinary', methods=["POST", "GET"])
+
+def to_binary():
+    
+    x = int(request.form['integer'])
+    
+    #check if input is 0
+    if int(x) == 0:
+        flash("binary code: 0")
+    
+        return render_template('index.html')
+    
+    #if not 0, establish number of digits in the desired binary 
+    n = (int(math.log(x, 2)) + 1)
+    b = [0] * n
+    #print(b)
+    
+    #insert 1 to first slot to represent first placeholder
+    b[0] = 1
+
+    #calculate initial remainder
+    rem = x - (2**(len(b)-1))
+    
+    #distribute remainder among other digits in list b
+    if rem != 0:
+        for i in reversed(range(0,((len(b)-1)))): 
+            if rem >= 2**i:
+                b[-(i+1)] = 1
+                rem = rem - 2**i
+    
+    #convert output from list to string
+    output = ''
+    for i in b:
+        output += str(i)
+        
+    flash("binary code: " + output)
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
